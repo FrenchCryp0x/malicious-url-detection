a2ae4d41378cd7a39c21fbb6217d36771e49d3f1
r3dkahili <119142862+FrenchCryp0x@users.noreply.github.com>
2025-04-30 16:46:00 +1000

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from huggingface_hub import login
 import os
 
 # Load Hugging Face token from Streamlit secrets
-hf_token = st.secrets["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"]
+hf_token = st.secrets["HUGGINGFACE_TOKEN"]
 login(hf_token)
 
 # Load model and tokenizer using token
